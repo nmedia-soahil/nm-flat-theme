438579e6cfc1dd02e9a8a48b7636851f824624f6
sohail-ahmad-shan <sohailahmadshan@gmail.com>
2016-05-15 21:07:57 +0500

    adding single.php and page.php

---
+++ page.php
@@ -0,0 +1,36 @@
+<?php get_header(); ?>
+
+<div id="templatemo_content_wrapper">
+	<div id="templatemo_content">
+    
+    	<div id="column_w530">
+        	
+        	<?php
+        	if( have_posts() ){
+        	  
+        	  while( have_posts() ){
+        	    
+        	    the_post();
+        	    ?>
+        	    
+        	    <div class="header_02"><?php the_title();?></div>
+        	    <span><?php the_date(); ?></span>
+        	    
+        	    <?php the_content(); ?>
+        	    
+        	  <?php
+        	  }
+        	  
+        	}	
+        	?>
+        	
+        	<div class="cleaner"></div>
+        </div>
+        
+        <?php get_sidebar(); ?>
+    
+    	<div class="cleaner"></div>
+    </div> <!-- end of content wrapper -->
+</div> <!-- end of content wrapper -->
+
+<?php get_footer(); ?>
